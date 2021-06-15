b003dae8fd082c4b4823f9fea581c10c25e6b587
Aqif Hamid <aqifhamid8@gmail.com>
2021-06-15 16:06:29 +0200

    Uncommented mvn commands from the startup script.

--- start_app.sh
+++ start_app.sh
@@ -1,9 +1,9 @@
 # !/bin/sh
 echo "Building project jar..."
-# mvn dependency:resolve
-# mvn verify
-# mvn clean package
-# cp ./ETLPipeline/target/ETLPipeline-1.0-SNAPSHOT.jar ./volumes/spark_app/ETLPipeline-1.0-SNAPSHOT.jar
-# cp ./KafkaProducer/target/KafkaProducer-1.0-SNAPSHOT-jar-with-dependencies.jar ./volumes/spark_app/KafkaProducer-1.0-SNAPSHOT-jar-with-dependencies.jar
+mvn dependency:resolve
+mvn verify
+mvn clean package
+cp ./ETLPipeline/target/ETLPipeline-1.0-SNAPSHOT.jar ./volumes/spark_app/ETLPipeline-1.0-SNAPSHOT.jar
+cp ./KafkaProducer/target/KafkaProducer-1.0-SNAPSHOT-jar-with-dependencies.jar ./volumes/spark_app/KafkaProducer-1.0-SNAPSHOT-jar-with-dependencies.jar
 echo "Launching services..."
 docker-compose -f docker-compose.yml up -d --build
